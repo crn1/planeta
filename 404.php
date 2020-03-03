91f86113da0f700ce7b0ddc9039d37f73fff4194
crn1 <gluvajic@aol.com>
2020-03-03 17:59:06 +0000

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+	<main>
+		<article class='default-container'>
+			<header>
+				<h2 class='title-heading'>
+					404
+				</h2>
+			</header>
+			<p class='subtitle-heading'>
+				Page Not Found!
+			</p>
+		</article>
+	</main>
+<?php get_footer(); ?>
