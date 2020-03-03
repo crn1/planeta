188d85b8f13f2c71dc74b244cb3c4d0aa8cb9c66
crn1 <gluvajic@aol.com>
2020-03-03 19:43:20 +0000

    Added bones for archive.php

--- archive.php
+++ archive.php
@@ -1,12 +1,7 @@
 <?php get_header(); ?>
 	<main>
 		<article class='default-container'>
-			<?php if(have_posts()): while(have_posts()) : the_post(); ?>
-				<h2 class='title-heading'>
-					<?php the_title(); ?>
-				</h2>
-				<?php the_content(); ?>
-			<?php endwhile; endif; ?>
+			<?php echo get_template_part('template-parts/content', 'postcards'); ?>
 		</article>
 	</main>
 <?php get_footer(); ?>
