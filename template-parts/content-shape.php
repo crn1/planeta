dcd2e6423ae472c725bd25a7471f616053fb72cd
crn1 <gluvajic@aol.com>
2020-03-25 18:44:28 +0000

    Updates shapes list

--- template-parts/content-shape.php
+++ template-parts/content-shape.php
@@ -4,102 +4,154 @@
 
 	if($shape != 'none')
 	{
-		function generate_zigzag($d=5, $strength=90, $is_rev=false)
-		{
-			$result = '';
-			for($i = 0; $i <= 100; $i+=$d)
-			{
-				if($i % ($d*2) == 0)
-				{
-					if($is_rev) {
-						$result .= "${i}% 100%, ";
-					}else {
-						$result .= "${i}% ${strength}%, ";
-					}
-				}else{
-					if($is_rev) {
-						$result .= "${i}% ${strength}%, ";
-					}else {
-						$result .= "${i}% 100%, ";
-					}
-				}
-			}
-			return $result;
-		}
-
 		echo 'style="';
 		$strength = get_theme_mod("${setting}_strength", 10);
-		$strength = 100 - $strength;
 
 		switch($shape)
 		{
 			case 'none':
 				break;
 
-			case 'line-left':
-				echo "clip-path: polygon(0 0, 100% 0, 100% 100%, 0% ${strength}%);";
+			case 'rounded':
+				$strength /= 2;
+				echo "border-radius: ${strength}%;";
 				break;
 
-			case 'line-right':
-				echo "clip-path: polygon(0 0, 100% 0, 100% ${strength}%, 0% 100%);";
+			case 'line-top-left':
+				echo "clip-path: polygon(0 ${strength}%, 100% 0, 100% 100%, 0 100%);";
 				break;
 
-			case 'point':
-				echo "clip-path: polygon(50% 100%, 100% ${strength}%, 100% 0, 0 0, 0 ${strength}%);";
+			case 'line-top-right':
+				echo "clip-path: polygon(0 0, 100% ${strength}%, 100% 100%, 0 100%);";
 				break;
 
-			case 'circle':
+			case 'line-bottom-left':
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(0 0, 100% 0, 100% 100%, 0% ${strength_rev}%);";
+				break;
+
+			case 'line-bottom-right':
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(0 0, 100% 0, 100% ${strength_rev}%, 0% 100%);";
+				break;
+
+			case 'line-both-left':
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(0 0, 100% ${strength}%, 100% 100%, 0 ${strength_rev}%);";
+				break;
+
+			case 'line-both-right':
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(0 ${strength}%, 100% 0, 100% ${strength_rev}%, 0 100%);";
+				break;
+
+			case 'point-up':
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(0 ${strength}%, 50% 0, 100% ${strength}%, 100% 100%, 0% 100%);";
+				break;
+
+			case 'point-down':
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(50% 100%, 100% ${strength_rev}%, 100% 0, 0 0, 0 ${strength_rev}%);";
+				break;
+
+			case 'point-left':
+				echo "clip-path: polygon(${strength}% 0, 100% 0, 100% 100%, ${strength}% 100%, 0 51%);";
+				break;
+
+			case 'point-right':
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(0% 0%, ${strength_rev}% 0, 100% 50%, ${strength_rev}% 100%, 0% 100%);";
+				break;
+
+			case 'circle-up':
+				$strength = 100 - $strength;
 				echo "clip-path: circle(${strength}vh at 50% 0);";
 				break;
 
-			case 'zigzag-1':
-				$zigzag = generate_zigzag(5, $strength);
-				echo "clip-path: polygon(${zigzag}	100% 0, 0 0);";
+			case 'circle-down':
+				$strength = 100 - $strength;
+				echo "clip-path: circle(${strength}vh at 50% 100%);";
 				break;
 
-			case 'zigzag-2':
-				$zigzag = generate_zigzag(5, $strength, true);
-				echo "clip-path: polygon(${zigzag}	100% 0, 0 0);";
+			case 'circle-full':
+				$strength = 100 - $strength;
+				$strength /= 2;
+				echo "clip-path: circle(${strength}vh at 50% 50%);";
 				break;
 
-			case 'zigzag-3':
-				$zigzag = generate_zigzag(4, $strength);
-				echo "clip-path: polygon(${zigzag}	100% 0, 0 0);";
-				break;
-
-			case 'zigzag-4':
-				$zigzag = generate_zigzag(4, $strength, true);
-				echo "clip-path: polygon(${zigzag}	100% 0, 0 0);";
-				break;
-
-			case 'zigzag-5':
-				$zigzag = generate_zigzag(2, $strength);
-				echo "clip-path: polygon(${zigzag}	100% 0, 0 0);";
-				break;
-
-			case 'zigzag-6':
-				$zigzag = generate_zigzag(2, $strength, true);
-				echo "clip-path: polygon(${zigzag}	100% 0, 0 0);";
-				break;
-
-			case 'zigzag-7':
-				$zigzag = generate_zigzag(1, $strength);
-				echo "clip-path: polygon(${zigzag}	100% 0, 0 0);";
-				break;
-
-			case 'zigzag-8':
-				$zigzag = generate_zigzag(1, $strength, true);
-				echo "clip-path: polygon(${zigzag}	100% 0, 0 0);";
-				break;
-
-			case 'paper-left':
+			case 'paper-top-left':
 				$strength_rev = 100 - $strength;
-				echo "border-radius: 0% 100% 0% 100% / ${strength}% 0% 100% ${strength_rev}%";
+				echo "border-radius: 100% 0% 100% 0% / ${strength}% 100% 0% ${strength_rev}%";
 				break;
 
-			case 'paper-right':
+			case 'paper-top-right':
 				$strength_rev = 100 - $strength;
-				echo "border-radius: 100% 0% 100% 0% / 0% ${strength}% ${strength_rev}% 100%";
+				echo "border-radius: 0% 100% 0% 100% / 100% ${strength}% ${strength_rev}% 0%";
+				break;
+
+			case 'paper-bottom-left':
+				$strength_rev = 100 - $strength;
+				echo "border-radius: 0% 100% 0% 100% / ${strength_rev}% 0% 100% ${strength}%";
+				break;
+
+			case 'paper-bottom-right':
+				$strength_rev = 100 - $strength;
+				echo "border-radius: 100% 0% 100% 0% / 0% ${strength_rev}% ${strength}% 100%";
+				break;
+
+			case 'trapezoid-up':
+				$strength /= 2;
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(${strength}% 0, ${strength_rev}% 0, 100% 100%, 0% 100%);";
+				break;
+
+			case 'trapezoid-down':
+				$strength /= 2;
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(${strength}% 100%, ${strength_rev}% 100%, 100% 0, 0 0);";
+				break;
+
+			case 'rhombus':
+				$strength /= 2;
+				$strength_rev = 100 - $strength;
+				echo "clip-path: polygon(50% ${strength}%, ${strength_rev}% 50%, 50% ${strength_rev}%, ${strength}% 50%);";
+				break;
+
+			case 'frame':
+				$strength /= 2;
+				$strength_rev = 100 - $strength; ?>
+					clip-path: polygon(
+						0% 0%,
+						0% 100%,
+						<?php echo $strength; ?>% 100%,
+						<?php echo $strength; ?>% <?php echo $strength; ?>%,
+						<?php echo $strength_rev; ?>% <?php echo $strength; ?>%,
+						<?php echo $strength_rev; ?>% <?php echo $strength_rev; ?>%,
+						<?php echo $strength; ?>% <?php echo $strength_rev; ?>%,
+						<?php echo $strength; ?>% 100%,
+						100% 100%,
+						100% 0%
+					);
+				<?php
+				break;
+
+			case 'inset':
+				$strength /= 2;
+				echo "clip-path: inset(${strength}% ${strength}% ${strength}% ${strength}%);";
+				break;
+
+			case 'fancy':
+				$str_1 = rand(0, 100);
+				$str_1_rev = 100 - $str_1;
+				$str_2 = rand(0, 100);
+				$str_2_rev = 100 - $str_2;
+				$str_3 = rand(0, 100);
+				$str_3_rev = 100 - $str_3;
+				$str_4 = rand(0, 100);
+				$str_4_rev = 100 - $str_4;
+
+				echo "border-radius: ${str_1}% ${str_1_rev}% ${str_2}% ${str_2_rev}% / ${str_3}% ${str_4}% ${str_4_rev}% ${str_3_rev}%";
 				break;
 
 			default:
