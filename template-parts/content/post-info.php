2c3dec5ee31022af89a9af3009d4df5e4f85e614
crn1 <gluvajic@aol.com>
2020-04-10 08:41:47 +0200

    Minor post-info styling

--- template-parts/content/post-info.php
+++ template-parts/content/post-info.php
@@ -4,7 +4,7 @@
 ?>
 <div class='post-info'>
 	<div class='user'>
-		<?php echo $avatar; ?> <?php the_date(); ?> - <?php the_author() ?>
+		<?php echo $avatar; ?> <?php the_author(); ?> - <?php the_date(); ?>
 	</div>
 
 	<?php the_category(' / '); ?>
