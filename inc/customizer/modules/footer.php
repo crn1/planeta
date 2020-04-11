415c2bb36915bc3af292b433a36ef90a787e9875
crn1 <gluvajic@aol.com>
2020-04-11 11:14:31 +0200

    Added options for footer spacing

--- inc/customizer/modules/footer.php
+++ inc/customizer/modules/footer.php
@@ -2,11 +2,83 @@
 
 require_once(get_template_directory() . '/inc/customizer/video-background.php');
 
+Kirki::add_panel('footer_panel', array(
+	'title'       => esc_html__('Footer', 'planeta'),
+	'panel'       => 'modules_panel',
+));
+
+Kirki::add_section('footer_spacing', array(
+	'title'       => esc_html__('Spacing', 'planeta'),
+	'panel'       => 'footer_panel',
+));
+
+Kirki::add_field('planeta_config', array(
+	'type'					=> 'slider',
+	'label'					=> esc_html__('Minimum Height (vh)', 'planeta'),
+	'section'				=> "footer_spacing",
+	'settings'			=> "footer_min_height",
+	'default'				=> 10,
+	'choices'				=> array(
+		'min'						=> 0,
+		'max'						=> 100,
+		'step'					=> 1,
+	),
+	'transport'			=> 'auto',
+	'output'				=> array(
+		array(
+			'element'			=> '#main-footer',
+			'property'		=> 'min-height',
+			'units'				=> 'vh',
+		),
+	),
+));
+
+Kirki::add_field('planeta_config', array(
+	'type'					=> 'slider',
+	'label'					=> esc_html__('Padding Top (vh)', 'planeta'),
+	'section'				=> "footer_spacing",
+	'settings'			=> "footer_padding_top",
+	'default'				=> 10,
+	'choices'				=> array(
+		'min'						=> 0,
+		'max'						=> 100,
+		'step'					=> 1,
+	),
+	'transport'			=> 'auto',
+	'output'				=> array(
+		array(
+			'element'			=> '#main-footer',
+			'property'		=> 'padding-top',
+			'units'				=> 'vh',
+		),
+	),
+));
+
+Kirki::add_field('planeta_config', array(
+	'type'					=> 'slider',
+	'label'					=> esc_html__('Spacing Top (vh)', 'planeta'),
+	'section'				=> "footer_spacing",
+	'settings'			=> "footer_margin_top",
+	'default'				=> 10,
+	'choices'				=> array(
+		'min'						=> 0,
+		'max'						=> 30,
+		'step'					=> 1,
+	),
+	'transport'			=> 'auto',
+	'output'				=> array(
+		array(
+			'element'			=> '#main-footer',
+			'property'		=> 'margin-top',
+			'units'				=> 'vh',
+		),
+	),
+));
+
 planeta_add_video_background(
 	$section = 'footer',
-	$panel = 'modules_panel',
-	$bg_output = '#main-footer-overlay',
-	$name = 'Footer Background'
+	$panel = 'footer_panel',
+	$bg_output = '#main-footer-overlay'
 );
 
 ?>
