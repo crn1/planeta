72f1a578b4ae5f7fbcee253595bf6cadf75460a7
crn1 <gluvajic@aol.com>
2020-04-19 12:42:15 +0200

    Designed enter password form

--- functions.php
+++ functions.php
@@ -100,3 +100,27 @@ function init_scripts()
 }
 
 add_action('wp_enqueue_scripts', 'init_scripts');
+
+//Protected Posts
+add_filter('the_password_form', 'planeta_password_form');
+function planeta_password_form()
+{
+	global $post;
+	$site_url = get_option('siteurl');
+	$enter_password = esc_html__('Enter Password', 'planeta');
+	$output = "<form
+			id='protected-post-form'
+			action='${site_url}/wp-login.php?action=postpass'
+			method='POST'>
+		<input
+			type='password'
+			required
+			placeholder='${enter_password}'
+			name='post_password'
+		/>
+		<button type='submit'>
+			<i class='icofont-simple-right'></i>
+		</button>
+	</form>";
+	return $output;
+}
