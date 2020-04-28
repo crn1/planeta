37c2efb840b4645d9f930cdd7d98562528b0e376
crn1 <gluvajic@aol.com>
2020-04-28 13:00:18 +0200

    Designed Generic Sections

--- template-parts/preset/generic.php
+++ template-parts/preset/generic.php
@@ -1,3 +1,30 @@
 <?php
-	//Silence is golden
-?>
+
+$title = get_the_title();
+if(!empty($title)): ?>
+<h3 class='card-title'>
+	<?php echo $title; ?>
+</h3>
+<?php endif; ?>
+
+<?php
+$content = get_the_content();
+if(!empty($content)): ?>
+<p>
+	<?php echo $content; ?>
+</p>
+<?php endif; ?>
+
+<?php $hover_class = get_query_var('hover_class', 'hover-none'); ?>
+
+<p class='card-url'>
+	<span class='relative <?php echo $hover_class; ?>'>
+		<a
+				class='button-link'
+				href='<?php echo the_permalink(); ?>'>
+
+			<?php echo esc_html__('Read More...', 'planeta'); ?>
+
+		</a>
+	</span>
+</p>
