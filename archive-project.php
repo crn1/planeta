0d6a9ff4c6efde08bb819f394f2832f61eb851a4
crn1 <gluvajic@aol.com>
2020-05-29 19:34:26 +0200

    Added primitive archive page for Projects

---
+++ archive-project.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<?php
+	get_template_part('template-parts/navbar'); ?>
+
+<main id='page'>
+	<div class='search-title-container default-container'>
+		<h2>
+			<?php the_archive_title(); ?>
+		</h2>
+	</div>
+
+	<div class='default-container'>
+		<?php
+			get_template_part('template-parts/content/results'); ?>
+	</div>
+
+</main>
+<?php get_footer(); ?>
