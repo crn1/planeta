b0f46d51ce21e01058c835bdf95ffac27bc90b72
crn1 <gluvajic@aol.com>
2020-06-01 18:02:14 +0200

    Redesigned tags

--- template-parts/content/tags.php
+++ template-parts/content/tags.php
@@ -5,17 +5,22 @@
 	<?php
 
 		$tags = get_the_tags();
-		foreach($tags as $tag)
+		if(!empty($tags))
 		{
-			$tag_link = get_tag_link( $tag->term_id ); ?>
-			<a
-					rel='tag'
-					title='<?php echo esc_attr($tag->name); ?>'
-					href='<?php echo esc_url($tag_link); ?>'
-					class='title-tag button-link <?php echo sanitize_html_class($tag->slug); ?>'>
-				<?php echo esc_html($tag->name); ?>
-			</a>
-		<?php } ?>
+			esc_html_e('Tags:', 'planeta');
+			foreach($tags as $tag)
+			{
+				$tag_link = get_tag_link($tag->term_id); ?>
+				<a
+						rel='tag'
+						title='<?php echo esc_attr($tag->name); ?>'
+						href='<?php echo esc_url($tag_link); ?>'
+						class='title-tag <?php echo sanitize_html_class($tag->slug); ?>'>
+					<?php echo esc_html($tag->name); ?>
+				</a><?php echo $tag !== end($tags) ? ',' : ''; ?>
+			<?php
+			}
+		} ?>
 		</p>
 	<?php
 	}
